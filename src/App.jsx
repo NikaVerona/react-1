42d7334c48f9f98388655851a4712c9d8d44b08f
mkubera <rainteller@tutanota.de>
2022-02-23 13:51:44 +0100

    Updated README, component, test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Pierwszy komponent w React</h1>
+      <p>Nie taki diabeł straszny</p>
+    </>
+  );
+};
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import App from './App';
-import Enzyme, { render, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import App from "./App";
+import Enzyme, { render, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('Button tests', () => {
-    const wrapper = shallow(<App />);
+describe("Button tests", () => {
+  const wrapper = shallow(<App />);
 
-    it('Dodano poprawnie komponent App', () => {
-      expect(!!wrapper).toBe(true);
-    });
-    it('Dodano nagłówek z odpowiednim tekstem', () => {
-      expect(wrapper.find('h1').text()).toMatch('Pierwszy komponent w React');
-    });
-    it('Dodano parametr funkcji callback odpalanej na kliku w button (onBtnClick)', () => {
-      expect(wrapper.find('p').text()).toMatch('Nie taki diabeł straszny');
-    });
+  it("Dodano poprawnie komponent App", () => {
+    expect(!!wrapper).toBe(true);
   });
+  it("Dodano nagłówek z odpowiednim tekstem", () => {
+    expect(wrapper.find("h1").text()).toMatch("Pierwszy komponent w React");
+  });
+  it("Dodano paragraf z odpowiednim tekstem", () => {
+    expect(wrapper.find("p").text()).toMatch("Nie taki diabeł straszny");
+  });
+});
