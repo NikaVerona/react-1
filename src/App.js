cfb97c3a541197b05a57393fc910c751da1cd40d
NikaVerona <46203132+NikaVerona@users.noreply.github.com>
2024-06-27 17:08:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./reset.css";
-import "./style.css";
-import "./desctop.css";
-import Header from "./components/Header";
-import LandingSection from "./components/LandingSection";
-import Employees from "./components/Employees";
-import Services from "./components/Services";
-import Footer from "./components/Footer";
+import React from 'react';
+import './reset.css';
+import './style.css';
+import './desctop.css';
+import Header from './components/Header';
+import LandingSection from './components/LandingSection';
+import Employees from './components/Employees';
+import Services from './components/Services';
+import Footer from './components/Footer';
 
 function App() {
   return (
